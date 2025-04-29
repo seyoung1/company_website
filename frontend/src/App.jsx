a4bcda8dcee1abe9736bbbc6d8b28f96a1afc8c3
seyoung1 <yxxxung@naver.com>
2025-04-29 13:10:51 +0900

    ch1-4  네비게이션 바

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/Footer/Navbar/Navbar";
+
+import { BrowserRouter } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+  </BrowserRouter>
+  );
+}
+
+export default App;
